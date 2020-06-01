3f2d3e6d595c50956c9429b3aefe6b6575ca4f31
selvakumar <selva2242@gmail.com>
2020-06-02 03:38:59 +0530

    Updates

---
+++ precache-manifest.e02ff26b8a2ba2f613ae53fd95fb3b3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0318b78c445a23bb0a20071e4355314",
+    "url": "/rocketlane-rocket-grid-task/index.html"
+  },
+  {
+    "revision": "456224fda1f63cdbac9b",
+    "url": "/rocketlane-rocket-grid-task/static/css/main.c91f4f7b.chunk.css"
+  },
+  {
+    "revision": "38a1dcbc2e32207f3824",
+    "url": "/rocketlane-rocket-grid-task/static/js/2.b5135b46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rocketlane-rocket-grid-task/static/js/2.b5135b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456224fda1f63cdbac9b",
+    "url": "/rocketlane-rocket-grid-task/static/js/main.5bc028e6.chunk.js"
+  },
+  {
+    "revision": "fc447dac0c1a80d137f5",
+    "url": "/rocketlane-rocket-grid-task/static/js/runtime-main.1d0d4fef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rocketlane-rocket-grid-task/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rocketlane-rocket-grid-task/precache-manifest.e02ff26b8a2ba2f613ae53fd95fb3b3d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rocketlane-rocket-grid-task/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
